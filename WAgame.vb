d53b87b9f5d2db843398e23d92d049302f927e75
Hubert <69467708+hubert838@users.noreply.github.com>
2020-11-03 15:17:51 +0100

    Create WAgame.vb

---
+++ WAgame.vb
@@ -0,0 +1,24 @@
+While True
+   ...
+End While
+
+Do While True
+   Do Until False
+      Do
+        Do
+          ...
+        Loop Until False
+      Loop While True
+   Loop
+Loop
+
+For x As Double =-3.5 To 30
+   For y As Integer =10 To 0 Step -2
+      ...
+      ' Komentarz
+   Next ' Opcjonalnie Next y
+Next 'opcjonalnie Next x
+
+Class
+   ...
+End Class
